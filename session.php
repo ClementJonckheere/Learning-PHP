a6ee143d7144f9275f71008054263526647a5cc4
ClementJonckheere <91881425+ClementJonckheere@users.noreply.github.com>
2022-07-08 13:29:21 +0200

    ajout session

---
+++ session.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title> Learning PHP | Données </title>
+</head>
+<!-- BODY -->
+<body>
+
+<h1>Test session</h1>
+<a href="ouvrir_session.php">Ouvrir une session</a><br>
+<a href="afficher_session.php">Afficher variables de session</a><br>
+<a href="session_detruite.php">Détruire variables de session</a><br>
+
+
+
+
+
+</body>
+</html>
